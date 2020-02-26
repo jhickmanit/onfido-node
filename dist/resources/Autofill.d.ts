b6298e6fb9bedd69ca1ca36fba44272c37829379
Jeff Hickman <jeff.hickman@onfido.com>
2020-02-26 13:41:46 -0800

    catching undocumented changes with extractions

--- dist/resources/Autofill.d.ts
+++ dist/resources/Autofill.d.ts
@@ -25,11 +25,16 @@ export declare type ExtractedData = {
     mrzLine1?: string | null;
     mrzLine2?: string | null;
     mrzLine3?: string | null;
-    addressLine1?: string | null;
-    addressLine2?: string | null;
-    addressLine3?: string | null;
-    addressLine4?: string | null;
-    addressLine5?: string | null;
+    addressLine_1?: string | null;
+    addressLine_2?: string | null;
+    addressLine_3?: string | null;
+    addressLine_4?: string | null;
+    addressLine_5?: string | null;
+    documentNumber?: string | null;
+    documentType?: string | null;
+    issuingCountry?: string | null;
+    issuingDate?: string | null;
+    issuingState?: string | null;
 };
 export declare class Extractions extends Resource<ExtractionRequest> {
     constructor(axiosInstance: AxiosInstance);
